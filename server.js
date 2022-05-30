ce2c04028e3b988aa215d6accac1318a25f3f50c
Cole <deoncole77@gmail.com>
2022-05-30 19:09:58 -0400

    server connection created

---
+++ server.js
@@ -0,0 +1,24 @@
+// import express and mongoose for server connection
+const express = require('express');
+const mongoose = require('mongoose');
+
+// create a variable to access the server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// set up middleware for the server
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// connect to the mongoose database
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-thoughts', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries
+mongoose.set('debug', true);
+
+// activate the server
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
